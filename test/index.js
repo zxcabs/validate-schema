b0fe51b5bb2b4abdf95e614a09cbfa2eeded17d2
Evgeny Reznichenko <kusakyky@gmail.com>
2011-10-07 13:37:16 +0400

    valid additional properties

--- test/index.js
+++ test/index.js
@@ -17,6 +17,26 @@ module.exports = {
 			assert.isUndefined(err);
 		});
 	},
+	'optional params 2': function () {
+		var schema = new Schema({
+			$foo: 1,
+			$bar: 2,
+			baz: 3,
+			foobar: 4
+		});
+
+		schema.validate({foo: 1, bar: 2, baz: 3, foobar: 4}, function (err) {
+			assert.isUndefined(err);
+		});
+		
+		schema.validate({bar: 2,  baz: 3, foobar: 4}, function (err) {
+			assert.isUndefined(err);
+		});
+
+		schema.validate({baz: 3, foobar: 4}, function (err) {
+			assert.isUndefined(err);
+		});
+	},
 	'required params': function () {
 		var schema = new Schema({
 			foo: 1,
@@ -30,9 +50,38 @@ module.exports = {
 		schema.validate({bar: 2}, function (err) {
 			assert.strictEqual(err, "key: foo must be required");
 		});
+
+	},
+	'required params 2': function () {
+		var schema = new Schema({
+			$foo: 1,
+			$bar: 2,
+			baz: 3,
+			foobar: 4
+		});
+
+		schema.validate({foo: 1, bar: 2}, function (err) {
+			assert.strictEqual(err, "key: foobar must be required");
+		});
 		
-		schema.validate({foo: 1, bar: 2, baz: 3}, function (err) {
-			assert.strictEqual(err, "To many keys");
+		schema.validate({bar: 2}, function (err) {
+			assert.strictEqual(err, "key: foobar must be required");
+		});
+
+		schema.validate({foobar: 4}, function (err) {
+			assert.strictEqual(err, "key: baz must be required");
+		});
+		
+		schema.validate({foo: 1, bar:2, foobar: 4}, function (err) {
+			assert.strictEqual(err, "key: baz must be required");
+		});	
+
+		schema.validate({baz:3, foobar: 4}, function (err) {
+			assert.isUndefined(err);
+		});
+
+		schema.validate({foo:1, bar:2, baz:3, foobar: 4}, function (err) {
+			assert.isUndefined(err);
 		});
 	},
 	'additional params': function () {
@@ -52,9 +101,37 @@ module.exports = {
 		schema.validate({foo: 1, bar: 2, baz: 3}, function (err) {
 			assert.strictEqual(err, "To many keys");
 		});
-
+	
 		schema.validate({bar: 2, baz: 3}, function (err) {
-			assert.strictEqual(err, "key: baz should not be included");
+			assert.strictEqual(err, "To many keys");
 		});
-	}	
+	},
+	'additional params 2': function () {
+		var schema = new Schema({
+			$foo: 1,
+			$bar: 2,
+			baz: 3,
+			foobar: 4
+		});
+
+		schema.validate({baz: 3, foobar: 4}, function (err) {
+			assert.isUndefined(err);
+		});
+		
+		schema.validate({foo: 1, bar: 2, baz: 3, foobar: 4}, function (err) {
+			assert.isUndefined(err);
+		});
+		
+		schema.validate({baz: 3, foobar: 4, bar: 2}, function (err) {
+			assert.isUndefined(err);
+		});
+		
+		schema.validate({foo: 1, bar: 2, baz: 3, foobar: 4, booz: 5}, function (err) {
+			assert.strictEqual(err, "To many keys");
+		});	
+
+		schema.validate({baz: 3, foobar: 4, booz: 5}, function (err) {
+			assert.strictEqual(err, "To many keys");
+		});
+	}
 }
